b6c0d0cb6b3b1551de6bee25d7b90eece10b8d4b
Gumilar Fajar <gumilar.fajar@arkamaya.co.id>
2024-07-01 01:27:19 +0700

    Modified People

--- src/components/pages/People/People.tsx
+++ src/components/pages/People/People.tsx
@@ -1,20 +1,21 @@
-import { Card, CardContent, Container, Paper, Table, TableContainer } from "@mui/material"
+import { Button, ButtonGroup, Card, CardContent, Container, Paper, Table, TableContainer, TextField } from "@mui/material"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import ButtonPaginationFragment from "../../fragments/ButtonPaginationFragment"
 import TableBodyPeopleFragment from "../../fragments/TableBodyPeopleFragment"
 import TableHeadFragment from "../../fragments/TableHeadFragment"
-import IPeople from "../../../interfaces/IPeople"
 import config from "../../../config/app.json"
 import imgStartWars from "../../../../public/starwars.png"
+import { Search } from "@mui/icons-material"
 
 const People = () => {
-  const apiUrl = config.baseApiUrl+'people/'
-  const [people, setPeople] = useState<IPeople[] | null>(null)
+  const apiUrl = config.baseApiUrl + 'people/'
+  const [people, setPeople] = useState<any[] | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [count, setCount] = useState<number>(0)
-  const [url, setUrl] = useState<string>(apiUrl+"?page=1")
+  const [url, setUrl] = useState<string>(apiUrl + "?page=1")
   const limit: number = 10
+  const [search, setSearch] = useState<string>('') // Ensure setSearch uses string type
 
   useEffect(() => {
     setIsLoading(true) // Set loading state when making request
@@ -22,7 +23,7 @@ const People = () => {
       .get(url)
       .then((response) => response.data)
       .then((data) => {
-        var results: IPeople[] = data.results
+        var results: any[] = data.results
         setPeople(results)
         setCount(data.count)
         setIsLoading(false) // Set loading state to false after data is fetched
@@ -37,13 +38,29 @@ const People = () => {
     setUrl(urlParam)
   }
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value)
+  };
+
+  const handleSearch = () => {
+    setUrl(apiUrl + "?search=" + search)
+  };
+
   const listTableHeaderTitle: string[] = ["Name", "Films", "Species", "Starships", "Vehicle", "Detail"]
 
   return (
     <Container className="text-center py-10">
       <Card>
         <CardContent>
-          <p className="text-4xl mb-10"><img src={imgStartWars} className="w-48"/></p>
+          <div className="flex justify-between mb-5">
+            <img src={imgStartWars} alt="Star Wars Logo" className="w-48" />
+            <div>
+              <ButtonGroup>
+                <TextField label="Search..." onChange={handleInputChange} />
+                <Button onClick={handleSearch}><Search /></Button>
+              </ButtonGroup>
+            </div>
+          </div>
           {isLoading ? (
             <p>Loading...</p> // Display loading message while fetching data
           ) : (
@@ -66,4 +83,4 @@ const People = () => {
   )
 }
 
-export default People
+export default People;
